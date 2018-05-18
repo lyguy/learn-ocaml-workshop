80e73f67304f8da04c8ef382be75f625facb1d19
Yaron Minsky <yminsky@janestreet.com>
2018-05-18 04:55:54 -0400

    shortened problem

--- 02-exercises/01-introduction/problem.ml
+++ 02-exercises/01-introduction/problem.ml
@@ -1,25 +1,10 @@
-(* Welcome to Jane Street's OCaml challenges!
+(* Run [jbuilder runtest] to build this file and run test it contains
 
-   This exercise is just meant to familiarize you with the system.
+   Goal: fix the compilation error, and make the test pass.
+*)
+open Base
+open Stdio
 
-   Write OCaml code using your favorite text editor; if you aren't already
-   committed to one, we recommend Visual Studio Code.
+let () = printf "Hello, World!
 
-   To compile your code and run inline tests, run
-
-   [jbuilder runtest]
-
-   in a terminal session in this problem's directory.
-
-   (There are several other ways to invoke [jbuilder] that we will encounter
-   later. For an introduction, watch the excellent video tutorial about jbuilder
-   linked in the [README] in this repository's base directory).
-
-   Try building this code.
-
-   You should see a compilation error because it's missing the end quote. Add
-   the end quote and re-run. You should see that the code compiled and ran!
-
-   You can also execute code in utop directly. Try pasting this line of code
-   into utop and running it there. *)
-let () = Stdio.printf "Hello, World!
+let%test "1+1 is 2" = 1 + 1 = 3
