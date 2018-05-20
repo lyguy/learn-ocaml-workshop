c1cc212a2907f314a72cf901ba018ad2390e24c5
Yaron Minsky <yminsky@janestreet.com>
2018-05-20 14:50:10 +1000

    cut out the guts of the exercise

--- 04-bigger-projects/fuzzy-finder/functional/test/basic_fuzzy_tests.ml
+++ 04-bigger-projects/fuzzy-finder/functional/test/basic_fuzzy_tests.ml
@@ -4,37 +4,28 @@ open! Functional_fzf
 
 let%expect_test _ =
   let open Fuzzy in
-  let now = Time.epoch in
-  let p m =
-    let (w,_) = Model.widget_and_selected m ~now in
+  let now = ref Time.epoch in
+  let m = ref (Fuzzy.Model.create ~now:!now) in
+  let update f = m := (f !m) in
+  let set_now s =
+    now := Time.add Time.epoch (Time.Span.of_sec s)
+  in
+  let p () =
+    let (w,_) = Model.widget_and_selected !m ~now:!now in
     printf "\n%s" (Tty_text.Widget.to_string w)
   in
-  let m = Fuzzy.Model.create ~now in
-  p m;
-  [%expect{| |   > |}];
-  let m = handle_line m "abc" in
-  let m = handle_line m "ccc" in
-  let m = handle_line m "aaabc" in
-  p m;
+  p ();
+  [%expect{| 0s 0 |}];
+  update (fun m -> handle_line m "abc");
+  update (fun m -> handle_line m "ccc");
+  update (fun m -> handle_line m "aaabc");
+  p ();
   [%expect{|
-    abc
-    ccc
-    aaabc
-    |   > |}];
-  let (m,_) = handle_user_input m (Char 'a') in
-  p m;
+    0s 3 |}];
+  update (fun m -> handle_line m "cccc");
+  p ();
   [%expect{|
-    abc
-    aaabc
-    |   > a |}];
-  let (m,_) = handle_user_input m (Char 'a') in
-  p m;
-  [%expect{|
-    aaabc
-    |   > aa |}];
-  let (m,_) = handle_user_input m Backspace in
-  p m;
-  [%expect {|
-    abc
-    aaabc
-    |   > a |}]
+    0s 4 |}];
+  set_now 2.;
+  p ();
+  [%expect{| 2s 4 |}]
